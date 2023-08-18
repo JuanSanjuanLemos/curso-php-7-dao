d5179547822810388bcb5f776a0e02c6b5b4a816
JuanSanjuanLemos <juansanjuan.dev@gmail.com>
2023-08-18 11:50:16 -0300

    Criação dos metódos get

--- class/Usuario.php
+++ class/Usuario.php
@@ -45,6 +45,38 @@ class Usuario{
     }
   }
 
+  public static function getById($id){
+    $sql = new Sql("localhost","dbphp7");
+    return $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = :ID",array(":ID"=>$id));
+  }
+  public static function getByLogin($login){
+    $sql = new Sql("localhost","dbphp7");
+    return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :LOGIN ORDER BY deslogin",array(":LOGIN"=>"%".$login."%"));
+  }
+
+  public static function getList(){
+    $sql = new Sql("localhost","dbphp7");
+    return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin");
+  }
+
+  public function login($login,$password){
+    $sql = new Sql("localhost","dbphp7");
+    $result = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD",array(
+      ":LOGIN"=>$login,
+      ":PASSWORD"=>$password
+    ));
+    $resultArray = json_decode($result,true);
+    if($resultArray){
+      $row = $resultArray[0];
+      $this->setIdusuario($row['idusuario']);
+      $this->setDeslogin($row['deslogin']);
+      $this->setDessenha($row['dessenha']);
+      $this->setDtcadastro(new DateTime($row['dtcadastro']));
+    } else{
+      throw new Exception(("Login ou senha inválidos!"));
+    }
+  }
+
   public function __toString(){
     $data = array(
       "idusuario"=>$this->getIdusuario(),
--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php
 require_once("config.php");
-$root = new Usuario();
-$root->loadById(7);
-echo $root;
-
+// $root = new Usuario();
+// $root->loadById(7);
+// echo $root;
+// echo Usuario::getList();
+// echo Usuario::getById(1);
+// echo Usuario::getByLogin("Pedro");
+$usuario = new Usuario();
+$usuario->login("bug","123");
+echo $usuario;
 ?>
